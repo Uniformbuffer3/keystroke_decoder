706965c9016ca66a7b90bdc09c64fdba7c9cdf35
Fabio Sgamma <uniformbuffer3@gmail.com>
2021-10-04 21:49:18 +0200

    Keep working on it

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,86 @@
-use xkbcommon::xkb;
+pub use xkbcommon::xkb;
 pub use xkb::keysyms;
+pub use xkb::KeyDirection;
+
+#[derive(Debug,Clone)]
+pub struct Rmlvo {
+    rules: String,
+    model: String,
+    layout: String,
+    variant: String,
+    options: Option<String>,
+}
+impl Rmlvo {
+    pub fn new()->Self {
+        let mut rmlvo = Self::default();
+        rmlvo.detect_from_etc_default_keyboard();
+        rmlvo.detect_from_env();
+        rmlvo
+    }
+
+
+    fn detect_from_etc_default_keyboard(&mut self){
+        let file_data = match std::fs::read_to_string("/etc/default/keyboard"){
+            Ok(file_data)=>file_data,
+            Err(_)=>return
+        };
+
+        let rules_regex = regex::Regex::new(r#"\s*XKBRULES\s*="(.+)""#).unwrap();
+        let model_regex = regex::Regex::new(r#"\s*XKBMODEL\s*="(.+)""#).unwrap();
+        let layout_regex = regex::Regex::new(r#"\s*XKBLAYOUT\s*="(.+)""#).unwrap();
+        let variant_regex = regex::Regex::new(r#"\s*XKBVARIANT\s*="(.+)""#).unwrap();
+        let options_regex = regex::Regex::new(r#"\s*XKBOPTIONS\s*="(.+)""#).unwrap();
+
+        for line in file_data.lines() {
+            if let Some(capture) = rules_regex.captures(line) {
+                self.rules = capture.get(1).unwrap().as_str().to_string();
+            };
+            if let Some(capture) = model_regex.captures(line) {
+                self.model = capture.get(1).unwrap().as_str().to_string();
+            };
+            if let Some(capture) = layout_regex.captures(line) {
+                self.layout = capture.get(1).unwrap().as_str().split(',').next().unwrap().to_string();
+            };
+            if let Some(capture) = variant_regex.captures(line) {
+                self.variant = capture.get(1).unwrap().as_str().to_string();
+            };
+            if let Some(capture) = options_regex.captures(line) {
+                self.options = Some(capture.get(1).unwrap().as_str().to_string());
+            };
+        }
+    }
+
+    fn detect_from_env(&mut self){
+        for (var, value) in std::env::vars() {
+            match var.as_str() {
+                "XKB_DEFAULT_RULES"=>self.rules = value,
+                "XKB_DEFAULT_MODEL"=>self.model = value,
+                "XKB_DEFAULT_LAYOUT"=>self.layout = value,
+                "XKB_DEFAULT_VARIANT"=>self.variant = value,
+                "XKB_DEFAULT_OPTIONS"=>self.options = Some(value),
+                _=>()
+            }
+        }
+    }
+
+    pub fn rules(&self)->&String {&self.rules}
+    pub fn model(&self)->&String {&self.model}
+    pub fn layout(&self)->&String {&self.layout}
+    pub fn variant(&self)->&String {&self.variant}
+    pub fn options(&self)->&Option<String> {&self.options}
+}
+
+impl Default for Rmlvo {
+    fn default()->Self {
+        Self {
+            rules: String::new(),
+            model: String::new(),
+            layout: String::new(),
+            variant: String::new(),
+            options: None
+        }
+    }
+}
 
 /**
 This structure initialize the xkb components to decode key strokes to an abstract xkb representation,
@@ -7,63 +88,19 @@ making possible to handle multiple keyboards layouts.
 */
 pub struct KeystrokeDecoder {
     context: xkb::Context,
+    rmlvo: Rmlvo,
     keymap: xkb::Keymap,
-    pub(crate) state: xkb::State,
-
+    state: xkb::State,
     keystroke_stack: Vec<xkb::Keysym>
 }
 impl KeystrokeDecoder {
-    fn detect_keyboard_layout_from_env() -> Result<String, ()> {
-        for (var, value) in std::env::vars() {
-            if var == "XKB_DEFAULT_LAYOUT" {
-                return Ok(value);
-            }
-        }
-        Err(())
-    }
-
-    fn detect_keyboard_layout_from_file() -> Result<String, ()> {
-        let regex = regex::Regex::new(r"\s*XKBLAYOUT\s*=(.+)").unwrap();
-
-        let file_data = std::fs::read_to_string("/etc/default/keyboard").unwrap();
-        for line in file_data.lines() {
-            if let Some(capture) = regex.captures(line) {
-                return Ok(capture.get(1).unwrap().as_str().to_string());
-            };
-        }
-        Err(())
-    }
-
-    fn detect_keyboard_layout() -> Result<String, ()> {
-        //Try to detect from env
-        if let Ok(layout) = Self::detect_keyboard_layout_from_env() {
-            return Ok(layout);
-        }
-
-        //Try to detect from file
-        if let Ok(layout) = Self::detect_keyboard_layout_from_file() {
-            return Ok(layout);
-        }
-        Err(())
-    }
-
     pub fn new() -> Self {
         // Initializing the xkb context with no flags
         let context = xkb::Context::new(0);
 
-        // Detecting keyboard layout
-        let keyboard_layout = match Self::detect_keyboard_layout() {
-            Ok(keyboard_layout) => {
-                println!("Detected layout: {}", &keyboard_layout);
-                keyboard_layout
-            }
-            Err(_) => String::from(""),
-        };
-
-        // Initializing the keymap using empty values ("").
-        // This will make xkb detect automatically the system keymap.
-        let keymap = xkb::Keymap::new_from_names(&context, "", "", &keyboard_layout, "", None, 0)
-            .expect("Fauled to create keymap");
+        let rmlvo = Rmlvo::new();
+        let keymap = xkb::Keymap::new_from_names(&context, rmlvo.rules(), rmlvo.model(), rmlvo.layout(), rmlvo.variant(), rmlvo.options().clone(), 0)
+            .expect("Failed to create keymap");
 
         // Initializing the xkb state that will be used to decode keystrokes
         let state = xkb::State::new(&keymap);
@@ -72,6 +109,7 @@ impl KeystrokeDecoder {
 
         Self {
             context,
+            rmlvo,
             keymap,
             state,
             keystroke_stack
@@ -93,12 +131,26 @@ impl KeystrokeDecoder {
             }
         };
         let keycode = keycode + 8;
-        self.state.update_key(keycode, direction.into());
+        self.state.update_key(keycode,match direction{
+            KeyDirection::Up=>KeyDirection::Up,
+            KeyDirection::Down=>KeyDirection::Down,
+        });
 
         Keystrokes {
             state: &self.state,
             keycode,
             direction
+        }
+    }
+
+    pub fn layout(&self)->&String {self.rmlvo.layout()}
+    pub fn set_layout(&mut self, layout: String)->bool {
+        let mut rmlvo = self.rmlvo.clone();
+        rmlvo.layout = layout;
+        match xkb::Keymap::new_from_names(&self.context, rmlvo.rules(), rmlvo.model(), rmlvo.layout(), rmlvo.variant(), rmlvo.options().clone(), 0)
+        {
+            Some(keymap)=>{self.keymap = keymap;true}
+            None=>false
         }
     }
 
@@ -108,35 +160,12 @@ impl KeystrokeDecoder {
     pub fn is_logo_pressed(&self)->bool {self.state.mod_name_is_active(&xkb::MOD_NAME_CTRL, xkb::STATE_MODS_EFFECTIVE)}
     pub fn is_caps_lock_pressed(&self)->bool {self.state.mod_name_is_active(&xkb::MOD_NAME_CTRL, xkb::STATE_MODS_EFFECTIVE)}
     pub fn is_num_lock_pressed(&self)->bool {self.state.mod_name_is_active(&xkb::MOD_NAME_CTRL, xkb::STATE_MODS_EFFECTIVE)}
+
 }
 
 impl Default for KeystrokeDecoder {
     fn default() -> Self {
         Self::new()
-    }
-}
-
-
-#[derive(Clone,Copy,Debug)]
-pub enum KeyDirection {
-    Up,
-    Down
-}
-impl From<xkb::KeyDirection> for KeyDirection {
-    fn from(direction: xkb::KeyDirection)->Self {
-        match direction {
-            xkb::KeyDirection::Up=>Self::Up,
-            xkb::KeyDirection::Down=>Self::Down
-        }
-    }
-}
-
-impl Into<xkb::KeyDirection> for KeyDirection {
-    fn into(self)->xkb::KeyDirection {
-        match self {
-            Self::Up=>xkb::KeyDirection::Up,
-            Self::Down=>xkb::KeyDirection::Down
-        }
     }
 }
 
@@ -147,14 +176,20 @@ pub struct Keystrokes<'a> {
 }
 impl<'a> Keystrokes<'a> {
     /// Return the keystrokes as keysyms
-    pub fn as_keysym(&self) -> Vec<(xkb::Keysym,KeyDirection)> {
-        self.state.key_get_syms(self.keycode).into_iter().map(|key|(*key,self.direction.clone())).collect()
+    pub fn as_keysyms(&self) -> Vec<(xkb::Keysym,KeyDirection)> {
+        self.state.key_get_syms(self.keycode).iter().map(|key|{
+            let direction = match self.direction{
+                KeyDirection::Up=>KeyDirection::Up,
+                KeyDirection::Down=>KeyDirection::Down,
+            };
+            (*key,direction)
+        }).collect()
     }
     /// Return the keystrokes as chars. Only key down direction will be returned.
     pub fn as_chars(&self) -> Vec<char> {
         match self.direction
         {
-            KeyDirection::Up => {return Vec::new();}
+            KeyDirection::Up => Vec::new(),
             KeyDirection::Down => {
                 let chars: Vec<char> = self.state.key_get_utf8(self.keycode).chars().collect();
                 if self.is_shift_pressed() {chars.into_iter().map(|character|character.to_uppercase().into_iter().collect::<Vec<char>>()).flatten().collect()}
@@ -173,92 +208,12 @@ impl<'a> Keystrokes<'a> {
 
 
 #[test]
-fn test_automatic_layout_detection() {
-    println!("{}", KeystrokeDecoder::detect_keyboard_layout().expect("Failed to autodetect keyboard layout"));
+fn test_create(){
+    let decoder = KeystrokeDecoder::new();
+
+    println!("{:#?}",decoder.layout());
+    //keymap.leds().for_each(|item|println!("{:#?}",item));
+    //keymap.layouts().for_each(|item|println!("{:#?}",item));
+
 }
 
-
-#[test]
-fn test_gatherer() {
-    use input_gatherer::*;
-
-    //Creating the gatherer
-    let mut gatherer = InputGatherer::new();
-    //Creating the keyboard decoder
-    let mut keystroke_decoder = KeystrokeDecoder::new();
-
-    let start = std::time::Instant::now();
-    let mut running = true;
-    while running {
-        //Dispatching events
-        gatherer
-            .dispatch_new_events(|event, _config| match event {
-                InputEvent::NewSeat(seat) => {
-                    println!("Seat added: {:#?}", seat);
-                }
-                InputEvent::SeatChanged(seat) => {
-                    println!("Seat changed: {:#?}", seat);
-                }
-                InputEvent::SeatRemoved(seat) => {
-                    println!("Seat removed: {:#?}", seat);
-                }
-                InputEvent::Keyboard { seat: _, event } => {
-                    // Decoding keystrokes
-                    let keystrokes = keystroke_decoder.decode(event.key());
-
-                    //Decoding keys into chars
-                    for key in keystrokes.as_chars() {
-                        println!("{}", key);
-                    }
-                    //Decoding keys into keysym
-                    for key_and_direction in keystrokes.as_keysym() {
-                        println!("{:#?}",&key_and_direction);
-                        match key_and_direction {
-                            (keysyms::KEY_Escape,KeyDirection::Up) => {
-                                println!("Esc pressed, early exit");
-                                running = false;
-                            }
-                            _ => {}
-                        }
-                    }
-
-                }
-                InputEvent::PointerMotion { seat: _, event } => {
-                    println!("{:#?}", event);
-                }
-                InputEvent::PointerMotionAbsolute { seat: _, event } => {
-                    println!("{:#?}", event);
-                }
-                InputEvent::PointerButton { seat: _, event } => {
-                    println!("{:#?}", event);
-                }
-                InputEvent::PointerAxis { seat: _, event } => {
-                    println!("{:#?}", event);
-                }
-                InputEvent::TouchDown { seat: _, event } => {
-                    println!("{:#?}", event);
-                }
-                InputEvent::TouchMotion { seat: _, event } => {
-                    println!("{:#?}", event);
-                }
-                InputEvent::TouchUp { seat: _, event } => {
-                    println!("{:#?}", event);
-                }
-                InputEvent::TouchCancel { seat: _, event } => {
-                    println!("{:#?}", event);
-                }
-                InputEvent::TouchFrame { seat: _, event } => {
-                    println!("{:#?}", event);
-                }
-                InputEvent::Special(event) => {
-                    println!("{:#?}", event);
-                }
-            })
-            .unwrap();
-
-        //After 5 seconds the loop terminate and give the control back to the terminal
-        if start.elapsed().as_secs() >= 5 {
-            running = false;
-        }
-    }
-}
